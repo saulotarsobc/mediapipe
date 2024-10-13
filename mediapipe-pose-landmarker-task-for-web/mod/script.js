801f7e1b2805b74748ef76ec7cb54e1b9e00d08e
saulotarsobc <saulotarsobc@gmail.com>
2024-10-13 16:18:06 -0300

    mod

--- mediapipe-pose-landmarker-task-for-web/mod/script.js
+++ mediapipe-pose-landmarker-task-for-web/mod/script.js
@@ -24,8 +24,10 @@ const video = document.getElementById("webcam");
 const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement.getContext("2d");
 const drawingUtils = new DrawingUtils(canvasCtx);
+
 // Check if webcam access is supported.
 const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
+
 // If webcam supported, add event listener to button for when user
 // wants to activate it.
 if (hasGetUserMedia()) {
@@ -35,42 +37,52 @@ if (hasGetUserMedia()) {
 else {
     console.warn("getUserMedia() is not supported by your browser");
 }
+
 // Enable the live webcam view and start detection.
 function enableCam(event) {
     if (!poseLandmarker) {
         console.log("Wait! poseLandmaker not loaded yet.");
         return;
     }
+
     if (webcamRunning === true) {
         webcamRunning = false;
         enableWebcamButton.innerText = "ENABLE PREDICTIONS";
     }
+
     else {
         webcamRunning = true;
         enableWebcamButton.innerText = "DISABLE PREDICTIONS";
     }
+
     // getUsermedia parameters.
     const constraints = {
         video: true
     };
+
     // Activate the webcam stream.
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
         video.srcObject = stream;
         video.addEventListener("loadeddata", predictWebcam);
     });
 }
+
 let lastVideoTime = -1;
+
 async function predictWebcam() {
     canvasElement.style.height = videoHeight;
     video.style.height = videoHeight;
     canvasElement.style.width = videoWidth;
     video.style.width = videoWidth;
+
     // Now let's start detecting the stream.
     if (runningMode === "IMAGE") {
         runningMode = "VIDEO";
         await poseLandmarker.setOptions({ runningMode: "VIDEO" });
     }
+
     let startTimeMs = performance.now();
+
     if (lastVideoTime !== video.currentTime) {
         lastVideoTime = video.currentTime;
         poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
@@ -85,6 +97,7 @@ async function predictWebcam() {
             canvasCtx.restore();
         });
     }
+
     // Call this function again to keep predicting when the browser is ready.
     if (webcamRunning === true) {
         window.requestAnimationFrame(predictWebcam);
